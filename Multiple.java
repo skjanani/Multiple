ac26ef468bc225a020544da6b2458c7b7482f416
skjanani <41111202+skjanani@users.noreply.github.com>
2018-07-23 10:24:02 +0530

    Add files via upload

---
+++ Multiple.java
@@ -0,0 +1,17 @@
+package janani;
+import java.util.Scanner;
+public class Multiple {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int i,n=5,mul,a;
+System.out.println("enter the num");
+Scanner ja=new Scanner(System.in);
+a=ja.nextInt();
+for(i=1;i<=5;i++){
+	mul=a*i;
+	System.out.print(mul+" ");
+}
+	}
+
+}
